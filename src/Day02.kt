b22e17cd590838d5af575567456855df8ca20dff
nickriordan <nick@anamatica.com>
2023-12-02 16:08:20 +0000

    Minor clean up

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,25 +4,20 @@ fun main() {
 
     fun extract(col: String, s: String) = s.substringBefore(col).trim().split(' ').last().toIntOrNull() ?: 0
 
-    fun gameData(input: List<String>) =
-        input.map { line ->
-            line.split(":").let { (gameAndId, turnsInfo) ->
-                Game(id = gameAndId.drop(5).toInt(),
-                    turns = turnsInfo.split(';').let { s ->
-                        s.map { Turn(extract("red", it), extract("green", it), extract("blue", it)) }
-                    })
-            }
+    fun gameData(input: List<String>) = input.map { line ->
+        line.split(':').let { (gameAndId, turnsInfo) ->
+            Game(id = gameAndId.drop(5).toInt(),
+                turns = turnsInfo.split(';').let { s ->
+                    s.map { Turn(extract("red", it), extract("green", it), extract("blue", it)) }
+                })
         }
+    }
 
     fun part1(input: List<String>) =
-        gameData(input).filter { g ->
-            g.turns.all { it.red <= 12 && it.green <= 13 && it.blue <= 14 }
-        }.sumOf { it.id }
+        gameData(input).filter { g -> g.turns.all { it.red <= 12 && it.green <= 13 && it.blue <= 14 } }.sumOf { it.id }
 
     fun part2(input: List<String>) =
-        gameData(input).sumOf { g ->
-            g.turns.maxOf { it.red } * g.turns.maxOf { it.green } * g.turns.maxOf { it.blue }
-        }
+        gameData(input).sumOf { g -> g.turns.maxOf { it.red } * g.turns.maxOf { it.green } * g.turns.maxOf { it.blue } }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
