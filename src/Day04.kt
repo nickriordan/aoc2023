47395cc9f1df7a45e7ba214a4bd6aeab15141d37
nickriordan <nick@anamatica.com>
2023-12-04 14:15:37 +0000

    Simplified day 4 solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,7 +1,7 @@
 import kotlin.math.min
 
 fun main() {
-    fun readCards(lines: List<String>): Map<Int, Int> = lines.associate { line ->
+    fun getScores(lines: List<String>) = lines.associate { line ->
         line.drop(5).split(':').let { (id, rest) ->
             rest.split('|').map { it.trim().split("\\s+".toRegex()).map { n -> n.toInt() } }.let { r ->
                 id.trim().toInt() to r.first().toSet().intersect(r.last().toSet()).count()
@@ -9,21 +9,18 @@ fun main() {
         }
     }  // Map of Card id to count of correct for each card
 
-    fun part1(input: List<String>) =
-        readCards(input).toList().sumOf { listOf(0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512)[it.second] }
+    fun part1(scores: Map<Int, Int>) = scores.values.sumOf { listOf(0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512)[it] }
 
-    fun part2(input: List<String>): Int {
-        val scores = readCards(input)
-        fun cardsWon(id: Int) = if (scores[id]!! == 0) null else id + 1..min(id + scores[id]!!, scores.size)
-        fun collect(id: Int): Int = 1 + (cardsWon(id)?.sumOf { collect(it) } ?: 0)
+    fun part2(scores: Map<Int, Int>): Int {
+        fun collect(id: Int): Int = (id + 1..min(id + scores[id]!!, scores.size)).sumOf { collect((it)) } + 1
         return scores.keys.sumOf { collect(it) }
     }
 
-    val testInput = readInput("Day04_test")
+    val testInput = getScores(readInput("Day04_test"))
     check(part1(testInput) == 13)
     check(part2(testInput) == 30)
 
-    val input = readInput("Day04")
+    val input = getScores(readInput("Day04"))
     part1(input).println()
     part2(input).println()
 }
