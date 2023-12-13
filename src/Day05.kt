9a7c542b080f433ede4e454c7ab2bf3017394254
nickriordan <nick@anamatica.com>
2023-12-13 23:46:10 +0000

    Day 5 - a bit brute force but it works

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,10 +1,51 @@
 fun main() {
-    fun part1(input: List<String>) = input.size
 
-    fun part2(input: List<String>) = input.size
+    fun makeLookup(input: List<String>, sectionName: String): (Long) -> Long {
+        data class Lookup(val srcRange: LongRange, val offset: Long)
+
+        val lookup =
+            input.asSequence().dropWhile { line ->
+                line != "$sectionName map:"
+            }.drop(1).takeWhile { line ->
+                line.isNotEmpty()
+            }.map { line ->
+                line.parseLongs().let { (destStart, srcStart, length) ->
+                    Lookup(srcStart..<srcStart + length, destStart - srcStart)
+                }
+            }.sortedBy { it.srcRange.first }.toList()
+
+        return { v: Long -> lookup.find { v in it.srcRange }?.run { v + offset } ?: v }
+    }
+
+    fun makeLookups(vararg lookups: (Long) -> Long) = { v: Long -> lookups.fold(v) { r, f -> f(r) } }
+
+    fun inputToLookup(input: List<String>) = makeLookups(
+        makeLookup(input, "seed-to-soil"),
+        makeLookup(input, "soil-to-fertilizer"),
+        makeLookup(input, "fertilizer-to-water"),
+        makeLookup(input, "water-to-light"),
+        makeLookup(input, "light-to-temperature"),
+        makeLookup(input, "temperature-to-humidity"),
+        makeLookup(input, "humidity-to-location")
+    )
+
+    fun part1(input: List<String>): Long {
+        val lookup = inputToLookup(input)
+        val seeds = input.first().drop(7).parseLongs()
+        return seeds.minOf { seed -> lookup(seed) }
+    }
+
+    fun part2(input: List<String>): Long {
+        val lookup = inputToLookup(input)
+        val seeds = input.first().drop(7).parseLongs().windowed(2, 2) { (start, len) ->
+            LongRange(start, start + len)
+        }.asSequence().flatten()
+        return seeds.minOf { seed -> lookup(seed) }
+    }
 
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 35L)
+    check(part2(testInput) == 46L)
 
     val input = readInput("Day05")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,8 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.math.min
+import kotlin.math.max
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +21,24 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * extract a list of ints from this string
+ */
+fun String.parseInts() = this.trim().split(" ").map { it.toInt() }
+
+fun String.parseLongs() = this.trim().split(" ").map { it.toLong() }
+
+fun LongRange.intersection(other: LongRange): LongRange {
+    val first = max(this.first, other.first)
+    val last = min(this.last, other.last)
+    return if (first <= last) LongRange(first, last) else LongRange.EMPTY
+}
+
+fun main() {
+    println((1L..7L).intersection(5L..10L))
+    println((5L..10L).intersection(1L..7L))
+    println((1L..5L).intersection(7L..10L))
+    println((7L..10L).intersection(1L..6L))
+    println((47L..103L).intersection(98L..99L))
+}
