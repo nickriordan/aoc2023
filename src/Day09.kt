5eb07928c3ac135d91301b8886c452d413cc6a9d
nickriordan <nick@anamatica.com>
2024-01-19 18:46:08 +0000

    Day 9 solutions

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,10 +1,26 @@
 fun main() {
-    fun part1(input: List<String>) = input.size
 
-    fun part2(input: List<String>) = input.size
+    fun List<Int>.allZero() = this.all { it == 0 }
+
+    fun part1(input: List<String>): Int {
+        fun processData(data: List<Int>): Int =
+            if (data.allZero()) 0 else
+                processData(data.zipWithNext().map { it.second - it.first }).let { r -> (data.last() + r) }
+
+        return input.map { it.parseInts() }.sumOf { processData(it) }
+    }
+
+    fun part2(input: List<String>): Int {
+        fun processData(data: List<Int>): Int =
+            if (data.allZero()) 0 else
+                processData(data.zipWithNext().map { it.second - it.first }).let { r -> data.first() - r }
+
+        return input.map { it.parseInts() }.sumOf { processData(it) }
+    }
 
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 114)
+    check(part2(testInput) == 2)
 
     val input = readInput("Day09")
     part1(input).println()
---
+++ src/Day10.kt
@@ -0,0 +1,12 @@
+fun main() {
+    fun part1(input: List<String>) = input.size
+
+    fun part2(input: List<String>) = input.size
+
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day10")
+    part1(input).println()
+    part2(input).println()
+}
