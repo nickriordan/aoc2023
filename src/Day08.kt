797ef98a16890ed93808a4c5717a77b3f8b25c71
nickriordan <nick@anamatica.com>
2024-01-19 14:06:49 +0000

    Day 8 solutions

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,10 +1,56 @@
 fun main() {
-    fun part1(input: List<String>) = input.size
+    data class Node(val left: String, val right: String)
 
-    fun part2(input: List<String>) = input.size
+    fun readNetwork(input: List<String>) =
+        input.drop(2).associate { line ->
+            line.filterNot { it.isWhitespace() || it in "=()," }.let { s ->
+                s.substring(0..2) to Node(s.substring(3..5), s.substring(6..8))
+            }
+        }
+
+    fun instructionsSequence(input: List<String>): Sequence<Char> {
+        val instr = input.first().splitToSequence(" ").first()
+        var itr = instr.iterator()
+        return generateSequence {
+            if (!itr.hasNext())
+                itr = instr.iterator()
+
+            itr.next()
+        }
+    }
+
+    fun nodeSequence(initial: String, network: Map<String, Node>, instructions: Sequence<Char>): Sequence<String> {
+        var currentNode = initial
+        val iter = instructions.iterator()
+
+        return generateSequence {
+            currentNode = when (iter.next()) {
+                'L' -> network[currentNode]!!.left
+                'R' -> network[currentNode]!!.right
+                else -> throw IllegalStateException("unknown direction")
+            }
+            currentNode
+        }
+    }
+
+    fun part1(input: List<String>) =
+        nodeSequence("AAA", readNetwork(input), instructionsSequence(input)).takeWhile { it != "ZZZ" }.count() + 1
+
+    fun part2(input: List<String>): Long {
+        val network = readNetwork(input)
+        val initialNodes = network.keys.filter { it.last() == 'A' }
+
+        return findLCMOfListOfNumbers(initialNodes.map { initial ->
+            nodeSequence(initial, network, instructionsSequence(input)).takeWhile { it.last() != 'Z' }.count() + 1L
+        })
+    }
 
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
+    check(part1(testInput) == 6)
+
+    val testInput2 = readInput("Day08_test2")
+    check(part2(testInput2) == 6L)
 
     val input = readInput("Day08")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -42,3 +42,24 @@ fun main() {
     println((7L..10L).intersection(1L..6L))
     println((47L..103L).intersection(98L..99L))
 }
+
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
+    var result = numbers[0]
+    for (i in 1 until numbers.size) {
+        result = findLCM(result, numbers[i])
+    }
+    return result
+}
